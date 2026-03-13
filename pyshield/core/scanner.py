046a42b540ecadc33d22496c6dd37369837a94e9
Ddavidd123 <stosicdavid6@gmail.com>
2026-03-13 10:43:39 +0100

    Dodavanje funkcionalnosti skeneru za skeniranje direktorijuma

--- pyshield/core/scanner.py
+++ pyshield/core/scanner.py
@@ -1,7 +1,7 @@
 """
 Glavni skener za proveru fajlova, i da li sadrzi malwer
 """
-
+import os
 from pyshield.core.hasher import calculate_sha256
 from pyshield.detection.signatures import is_malware
 
@@ -33,4 +33,45 @@ def scan_file(file_path):
         "hash": file_hash,
         "is_malware": detected,
         "malware_name": malware_name,
+    }
+
+
+def scan_directory(directory_path):
+    """
+    Skeniranje fajlova u datom direktorijumu 
+    """
+
+    if not os.path.isdir(directory_path):
+        return {
+            "status": "error",
+            "message": "Directory not found",
+            "directory_path": directory_path,
+            "total_files": 0,
+            "malware_detected": 0,
+            "clean_files": 0,
+            "errors": 1,
+            "results": [],
+        }
+    results = []
+    # root koristim ignorisem _ - for root, _, files
+    for root, _, files in os.walk(directory_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            result = scan_file(file_path)
+            results.append(result)
+    
+    malware_detected = sum(1 for r in results if r["is_malware"])
+    errors = sum(1 for r in results if r["status"] == "error")
+    clean_files = sum(1 for r in results if r["status"] == "scanned" and not r["is_malware"])
+
+    return{
+        "status": "completed",
+        "message": "Directory scan completed",
+        "directory_path": directory_path,
+        "total_files": len(results),
+        "malware_detected": malware_detected,
+        "clean_files": clean_files,
+        "errors": errors,
+        "results": results,
+
     }
